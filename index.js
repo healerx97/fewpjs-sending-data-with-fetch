973e87d0ebddbb029ac7f31537864a0ef07eb894
Kenny Yoon <ghun.yoon@gmail.com>
2021-05-24 03:50:34 -0400

    commit tree d317ba37507e9f7df75f3ff4cf7cf12724b1f64c

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(userName, userMail) {
+    let formData = {
+        name: userName,
+        email: userMail
+    }
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+    return fetch(`http://localhost:3000/users`, configObj)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(object) {
+        document.body.innerHTML = object.id
+
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message
+    })
+}
+submitData('steve', 'steve@steve.com')
